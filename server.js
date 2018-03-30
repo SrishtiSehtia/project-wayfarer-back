5dba592c80730d332eaf77f298451401382c67b0
Srishti Sehtia <srishti0123@gmail.com>
2018-03-30 14:24:39 -0700

    CRUD cities api

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
 var express = require('express');
 var app = express();
 var db = require('./models');
+var bodyParser = require('body-parser');
 
-const cities = [
-      {
-        name: 'San Francisco',
-        country: 'USA'
-      },
-      {
-        name: 'Los Angeles',
-        country: 'USA'
-      },
-      {
-        name: 'New Delhi',
-        country: 'India'
-      }
-    ];
+// body parser config to accept our datatypes
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   res.send('This is the API for project Wayfarer!');
@@ -30,6 +19,7 @@ app.get('/api/cities', function(req, res) {
   });
 })
 
+//show city
 app.get('/api/cities/:id', function (req, res) {
   db.City.findOne({_id: req.params.id }, function(err, data) {
       res.json(data);
@@ -38,8 +28,34 @@ app.get('/api/cities/:id', function (req, res) {
 
 //create new city
 app.post('/api/cities', function (req, res) {
-
+  // create new city with form data (`req.body`)
+var newCity = new db.City({
+  name: req.body.name,
+  country: req.body.country
+  });
+  // save newCity to database
+  newCity.save(function(err, city){
+    if (err) {
+      return console.log("save error: " + err);
+    }
+    console.log("saved ", city.name);
+    // send back the city!
+    res.json(city);
+  });
 })
+
+// delete city
+app.delete('/api/cities/:id', function (req, res) {
+  // get city id from url params (`req.params`)
+  console.log('cities delete', req.params);
+  var cityId = req.params.id;
+  // find the index of the city we want to remove
+  db.City.findOneAndRemove({ _id: cityId })
+    .populate('post')
+    .exec(function (err, deletedCity) {
+      res.json(deletedCity);
+  });
+});
 
 app.listen(process.env.PORT || 5000, function () {
     console.log('Example app listening at http://localhost:5000/');
